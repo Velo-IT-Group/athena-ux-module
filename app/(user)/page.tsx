6bd233a9c5b3666782db0b150ee2ed7b983cccb7
Nick Black <nickblack@VELO-V201849.local>
2024-08-02 08:24:34 -0500

    removed a bunch of stuff

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,43 +1,10 @@
-import { DataItem, Overview } from './overview';
-import { groupBy } from 'lodash';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Table, TableCell, TableHeader, TableRow } from '@/components/ui/table';
-import { getInboundCalls } from '@/lib/twilio/read';
-import { type ChartConfig } from '@/components/ui/chart';
-import BarChart from '@/components/bar-chart';
-
-const chartConfig = {
-	desktop: {
-		label: 'This Month',
-		color: '#2563eb',
-	},
-	mobile: {
-		label: 'Last Month',
-		color: '#60a5fa',
-	},
-} satisfies ChartConfig;
-
-const chartData = [
-	{ month: 'January', desktop: 186, mobile: 80 },
-	{ month: 'February', desktop: 305, mobile: 200 },
-	{ month: 'March', desktop: 237, mobile: 120 },
-	{ month: 'April', desktop: 73, mobile: 190 },
-	{ month: 'May', desktop: 209, mobile: 130 },
-	{ month: 'June', desktop: 214, mobile: 140 },
-];
-import { DateRangePicker } from '@/components/ui/date-range-picker';
-import { getEvents } from '@/lib/twilio/taskrouter/helpers';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { auth } from '@/auth';
-import { cn } from '@/lib/utils';
 import { PhoneIncoming, Smile } from 'lucide-react';
 import Metric from './metric';
 import { ServiceLevel } from './service-level';
 import { CallVolume } from './call-volume';
 
 export default async function Page({ searchParams }: { searchParams: any }) {
-	const session = await auth();
-
 	return (
 		<main className='grid gap-3 p-3'>
 			<Card>

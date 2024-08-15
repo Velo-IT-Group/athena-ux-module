d4b2d934a8b033f45b953863529566a82a606c2f
nickblack26 <nicholas.black98@icloud.com>
2024-08-15 09:52:42 -0500

    using contact list component to render table

--- app/(user)/contacts/page.tsx
+++ app/(user)/contacts/page.tsx
@@ -1,17 +1,20 @@
-import { getContacts } from '@/lib/manage/read';
-import { DataTable } from '@/components/ui/data-table';
-import { columns } from '@/components/table-columns/contact';
+import { getCompanies } from '@/lib/manage/read';
+import ContactList from '@/components/lists/contact-list';
+import { Suspense } from 'react';
+import TableSkeleton from '@/components/ui/data-table/skeleton';
 
 type Props = {};
 
 const Page = async (props: Props) => {
-	const contacts = await getContacts({
-		pageSize: 1000,
-		orderBy: { key: 'firstName' },
-		conditions: [{ parameter: { inactiveFlag: false } }],
-		childConditions: [{ parameter: { 'types/id': 17 } }],
-		fields: ['id', 'firstName', 'lastName', 'company', 'communicationItems', 'defaultPhoneNbr', 'types'],
-	});
+	const [companies] = await Promise.all([
+		getCompanies({
+			conditions: [{ parameter: { 'status/id': 1 } }],
+			childConditions: [{ parameter: { 'types/id': 1 } }],
+			orderBy: { key: 'name', order: 'asc' },
+			fields: ['id', 'name'],
+			pageSize: 1000,
+		}),
+	]);
 
 	return (
 		<main className='p-3'>
@@ -20,11 +23,18 @@ const Page = async (props: Props) => {
 			</header>
 
 			<section>
-				<DataTable
-					data={contacts}
-					columns={columns}
-					meta={{ filterKey: 'firstName' }}
-				/>
+				<Suspense fallback={<TableSkeleton />}>
+					<ContactList
+						type='table'
+						params={{
+							orderBy: { key: 'firstName' },
+							conditions: [{ parameter: { inactiveFlag: false } }],
+							childConditions: [{ parameter: { 'types/id': 17 } }, { parameter: { 'types/id': 21 } }],
+							fields: ['id', 'firstName', 'lastName', 'company', 'communicationItems', 'defaultPhoneNbr'],
+						}}
+						facetedFilters={[{ accessoryKey: 'company', items: companies }]}
+					/>
+				</Suspense>
 			</section>
 		</main>
 	);

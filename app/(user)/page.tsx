2705339fa880d6e2215437285e0d14dfe7f29e78
nickblack26 <nicholas.black98@icloud.com>
2024-08-15 09:52:18 -0500

    cleaned up unused or commented code

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,10 +1,7 @@
 import ConversationContactDetail from './conversations/[id]/conversation-contact-detail';
 import ConversationDetails from './conversations/[id]/active-conversation';
-import ConversationCompanyDetail from './conversations/[id]/conversation-company-detail';
 import { getTask } from '@/lib/twilio/taskrouter/helpers';
 import { TaskInstance } from 'twilio/lib/rest/taskrouter/v1/workspace/task';
-import { Suspense } from 'react';
-import { Skeleton } from '@/components/ui/skeleton';
 import { getContactCommunications } from '@/lib/manage/read';
 import { auth } from '@/auth';
 
@@ -24,14 +21,8 @@ export default async function Page({
 	const contactId = searchParams.contactId ? parseInt(searchParams.contactId) : undefined;
 	const contactCommunications = await getContactCommunications(contactId ?? session?.user?.contactId ?? 0);
 
-	console.log(session?.user.contactId);
-
-	console.log(session?.user.referenceId);
-
 	return (
 		<main className='grid grid-cols-[1fr_3fr] h-full w-full'>
-			{/* <Suspense fallback={<Skeleton className='h-full w-full' />}>
-			</Suspense> */}
 			<ConversationContactDetail
 				companyId={companyId ?? 250}
 				contactId={contactId ?? session?.user?.contactId ?? 10}

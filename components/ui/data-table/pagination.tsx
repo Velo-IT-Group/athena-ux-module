2beb4a9e2f86d4ca4e65dc83b21731270e8df034
nickblack26 <nicholas.black98@icloud.com>
2024-08-15 09:52:04 -0500

    removed unecessary width class

--- components/ui/data-table/pagination.tsx
+++ components/ui/data-table/pagination.tsx
@@ -1,4 +1,4 @@
-import { ChevronLeftIcon, ChevronRightIcon, ChevronsUp, ChevronsRight, ChevronsLeft } from 'lucide-react';
+import { ChevronLeftIcon, ChevronRightIcon, ChevronsRight, ChevronsLeft } from 'lucide-react';
 import { Table } from '@tanstack/react-table';
 
 import { Button } from '@/components/ui/button';
@@ -14,6 +14,7 @@ export function DataTablePagination<TData>({ table }: DataTablePaginationProps<T
 			<div className='flex-1 text-sm text-muted-foreground'>
 				{table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} row(s) selected.
 			</div>
+
 			<div className='flex items-center space-x-6 lg:space-x-8'>
 				<div className='flex items-center space-x-2'>
 					<p className='text-sm font-medium'>Rows per page</p>
@@ -38,9 +39,11 @@ export function DataTablePagination<TData>({ table }: DataTablePaginationProps<T
 						</SelectContent>
 					</Select>
 				</div>
-				<div className='flex w-[100px] items-center justify-center text-sm font-medium'>
+
+				<div className='flex items-center justify-center text-sm font-medium'>
 					Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
 				</div>
+
 				<div className='flex items-center space-x-2'>
 					<Button
 						variant='outline'
@@ -51,6 +54,7 @@ export function DataTablePagination<TData>({ table }: DataTablePaginationProps<T
 						<span className='sr-only'>Go to first page</span>
 						<ChevronsLeft />
 					</Button>
+
 					<Button
 						variant='outline'
 						size='icon'
@@ -60,6 +64,7 @@ export function DataTablePagination<TData>({ table }: DataTablePaginationProps<T
 						<span className='sr-only'>Go to previous page</span>
 						<ChevronLeftIcon />
 					</Button>
+
 					<Button
 						variant='outline'
 						size='icon'
@@ -69,6 +74,7 @@ export function DataTablePagination<TData>({ table }: DataTablePaginationProps<T
 						<span className='sr-only'>Go to next page</span>
 						<ChevronRightIcon />
 					</Button>
+
 					<Button
 						variant='outline'
 						size='icon'

806f98c1183647cfe1f5fd864067e67cf6f64c8b
nickblack26 <nicholas.black98@icloud.com>
2024-08-15 09:53:02 -0500

    added table component

--- components/lists/contact-list.tsx
+++ components/lists/contact-list.tsx
@@ -2,14 +2,19 @@ import { Contact, ReferenceType } from '@/types/manage';
 import { Conditions } from '@/utils/manage/params';
 import React, { ReactNode } from 'react';
 import { Combobox } from '../ui/combobox';
-import { getContacts } from '@/lib/manage/read';
+import { getAllContacts } from '@/lib/manage/read';
 import { Button } from '../ui/button';
 import { User } from 'lucide-react';
+import { DataTable } from '../ui/data-table';
+import { columns } from '../table-columns/contact';
+import { FacetedFilter } from '../ui/data-table/toolbar';
 
 type Props = {
 	type: 'table' | 'combobox' | 'select';
 	defaultValue?: ReferenceType;
 	params?: Conditions<Contact>;
+	facetedFilters?: FacetedFilter<Contact>[];
+	hidePagination?: boolean;
 	children?: ReactNode;
 };
 
@@ -22,11 +27,23 @@ const ContactList = async ({
 		orderBy: { key: 'firstName' },
 	},
 	children,
+	hidePagination,
+	facetedFilters,
 }: Props) => {
-	const contacts = await getContacts(params);
+	const { contacts, count } = await getAllContacts(params);
 
 	return (
 		<>
+			{type === 'table' && (
+				<DataTable
+					data={contacts}
+					columns={columns}
+					facetedFilters={facetedFilters}
+					count={count}
+					meta={{ filterKey: 'firstName' }}
+					hidePagination={hidePagination}
+				/>
+			)}
 			{type === 'combobox' && (
 				<Combobox
 					items={

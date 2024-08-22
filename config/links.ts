25b99a97de893d2de7ca41ce20d1abf214603d89
Nick Black <nickblack@VELO-V201849.local>
2024-08-22 15:03:40 -0500

    purged links to start over

--- config/links.ts
+++ config/links.ts
@@ -1,17 +1,4 @@
 import { NavItem, NavItemWithChildren } from '@/types/nav';
-import {
-	Building2,
-	FileText,
-	Headset,
-	Home,
-	LineChart,
-	ListEnd,
-	Notebook,
-	Route,
-	Settings,
-	Tags,
-	Workflow,
-} from 'lucide-react';
 
 export interface LinksConfig {
 	modules: NavItem[];
@@ -21,92 +8,8 @@ export interface LinksConfig {
 }
 
 export const linksConfig: LinksConfig = {
-	modules: [
-		{
-			title: 'User Experience',
-			href: '/ux',
-			icon: Headset,
-		},
-		{
-			title: 'Proposal',
-			href: '/proposal',
-			icon: FileText,
-		},
-	],
+	modules: [],
 	mainNav: [],
-	sidebarNav: [
-		{
-			title: 'Dashboard',
-			href: '/',
-			icon: Home,
-		},
-		{
-			title: 'Contacts',
-			href: '/contacts',
-			icon: Notebook,
-		},
-		{
-			title: 'Tickets',
-			href: '/tickets',
-			icon: Tags,
-		},
-		{
-			title: 'Companies',
-			href: '/companies',
-			icon: Building2,
-		},
-		{
-			title: 'Analytics',
-			href: '/analytics',
-			icon: LineChart,
-		},
-		{
-			title: 'Settings',
-			href: '/settings',
-			icon: Settings,
-		},
-	],
-	settingsNav: [
-		{
-			title: 'Getting Started',
-			items: [
-				{
-					title: 'General',
-					href: '/settings',
-					icon: Home,
-					items: [],
-				},
-				{
-					title: 'Paths',
-					href: '/settings/paths',
-					icon: Route,
-					items: [],
-				},
-				{
-					title: 'Queues',
-					href: '/settings/queues',
-					icon: ListEnd,
-					items: [],
-				},
-				{
-					title: 'Workflows',
-					href: '/settings/workflows',
-					icon: Workflow,
-					items: [],
-				},
-				{
-					title: 'Schedule',
-					href: '/settings/schedule',
-					icon: Home,
-					items: [],
-				},
-				{
-					title: 'Caller IDs',
-					href: '/settings/caller-ids',
-					icon: Home,
-					items: [],
-				},
-			],
-		},
-	],
+	sidebarNav: [],
+	settingsNav: [],
 };

521f280f03ee807cf9e9ab7e1db222356b67523d
Nick Black <nickblack@VELO-V201849.local>
2024-08-22 16:27:27 -0500

    creating the client asynchronously

--- utils/twilio/index.ts
+++ utils/twilio/index.ts
@@ -1,7 +1,7 @@
 'use server';
 import { Twilio } from 'twilio';
 
-export const createClient = () =>
-	new Twilio(process.env.TWILIO_API_KEY_SID, process.env.TWILIO_API_KEY_SECRET, {
+export const createClient = async () =>
+	await new Twilio(process.env.TWILIO_API_KEY_SID, process.env.TWILIO_API_KEY_SECRET, {
 		accountSid: process.env.TWILIO_ACCOUNT_SID,
 	});

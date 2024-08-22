732f8f8b3eb472f4a15fda4fa151810d57e1b32f
Nick Black <nickblack@VELO-V201849.local>
2024-08-22 15:07:47 -0500

    unified links from links config

--- components/side-nav.tsx
+++ components/side-nav.tsx
@@ -1,72 +1,26 @@
 'use client';
+import { useState } from 'react';
 import { buttonVariants } from '@/components/ui/button';
-import { Building2, Home, LineChart, LucideIcon, Notebook, Settings, Tags } from 'lucide-react';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { ScrollArea } from './ui/scroll-area';
 import { ResizablePanel } from './ui/resizable';
 import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
-import { useEffect, useState } from 'react';
 import TaskList from './lists/task-list';
 import { Separator } from './ui/separator';
 import { usePathname } from 'next/navigation';
-import { ModuleSwitcher } from './module-switcher';
 import { linksConfig } from '@/config/links';
-import { useWorker } from '@/providers/worker-provider';
 import SidebarActivityList from './sidebar-activity-list';
+import FavIcon from './icons/favicon';
 
 type Props = {
 	isDefaultCollapsed: boolean;
 	defaultLayout?: number[];
 };
 
-export type NavLink = {
-	name: string;
-	href: string;
-	icon: LucideIcon;
-};
-
-const links: NavLink[] = [
-	{
-		name: 'Dashboard',
-		href: '/',
-		icon: Home,
-	},
-	{
-		name: 'Contacts',
-		href: '/contacts',
-		icon: Notebook,
-	},
-	{
-		name: 'Tickets',
-		href: '/tickets',
-		icon: Tags,
-	},
-	{
-		name: 'Companies',
-		href: '/companies',
-		icon: Building2,
-	},
-	{
-		name: 'Analytics',
-		href: '/analytics',
-		icon: LineChart,
-	},
-	{
-		name: 'Settings',
-		href: '/settings',
-		icon: Settings,
-	},
-];
-
 const SideNav = ({ isDefaultCollapsed, defaultLayout = [15, 32, 48] }: Props) => {
 	const [isCollapsed, setIsCollapsed] = useState(isDefaultCollapsed);
 	const pathname = usePathname();
-	const { worker } = useWorker();
-
-	useEffect(() => {
-		// setIsCollapsed(isDefaultCollapsed);
-	}, []);
 
 	return (
 		<ResizablePanel
@@ -87,19 +41,16 @@ const SideNav = ({ isDefaultCollapsed, defaultLayout = [15, 32, 48] }: Props) =>
 			<ScrollArea className='flex flex-col h-screen'>
 				<div
 					data-collapsed={isCollapsed}
-					className='group flex flex-col space-y-1.5'
+					className='group flex flex-col'
 				>
 					<div className={cn('flex h-12 items-center justify-center py-0.5', isCollapsed ? 'h-12' : 'px-1.5')}>
-						<ModuleSwitcher
-							isCollapsed={isCollapsed}
-							links={linksConfig.modules}
-						/>
+						<FavIcon />
 					</div>
 
 					<Separator />
 
 					<nav className='grid gap-1.5 px-1.5 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-1.5 group-[[data-collapsed=true]]:py-1.5'>
-						{links.map((link, index) =>
+						{linksConfig.sidebarNav.map((link, index) =>
 							isCollapsed ? (
 								<Tooltip
 									key={index}
@@ -107,16 +58,14 @@ const SideNav = ({ isDefaultCollapsed, defaultLayout = [15, 32, 48] }: Props) =>
 								>
 									<TooltipTrigger asChild>
 										<Link
-											href={link.href}
+											href={link.href ?? '#'}
 											className={cn(
 												buttonVariants({ variant: pathname === link.href ? 'default' : 'ghost', size: 'icon' }),
 												'h-9 w-9'
-												// link.variant === 'default' &&
-												// 	'dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white'
 											)}
 										>
-											<link.icon className='h-3.5 w-3.5' />
-											<span className='sr-only'>{link.name}</span>
+											{link.icon && <link.icon className='h-3.5 w-3.5' />}
+											<span className='sr-only'>{link.title}</span>
 										</Link>
 									</TooltipTrigger>
 
@@ -124,33 +73,20 @@ const SideNav = ({ isDefaultCollapsed, defaultLayout = [15, 32, 48] }: Props) =>
 										side='right'
 										className='flex items-center gap-3'
 									>
-										{link.name}
-										{/* {link.name && <span className='ml-auto text-muted-foreground'>{link.name}</span>} */}
+										{link.title}
 									</TooltipContent>
 								</Tooltip>
 							) : (
 								<Link
 									key={index}
-									href={link.href}
+									href={link.href ?? '#'}
 									className={cn(
 										buttonVariants({ variant: pathname === link.href ? 'default' : 'ghost', size: 'sm' }),
-										// link.variant === 'default' &&
-										// 	'dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white',
 										'justify-start'
 									)}
 								>
-									<link.icon className='mr-1.5 h-3.5 w-3.5' />
-									{link.name}
-									{/* {link.name && (
-										<span
-											className={cn(
-												'ml-auto'
-												// link.variant === 'default' && 'text-background dark:text-white'
-											)}
-										>
-											{link.name}
-										</span>
-									)} */}
+									{link.icon && <link.icon className='mr-1.5 h-3.5 w-3.5' />}
+									{link.title}
 								</Link>
 							)
 						)}

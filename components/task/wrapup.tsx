e6fcfe5ad1c67ea3c718430712cf8be6d7f2e149
Nick Black <nickblack@VELO-V201849.local>
2024-08-23 10:56:10 -0500

    added more context to the wrapup task

--- components/task/wrapup.tsx
+++ components/task/wrapup.tsx
@@ -1,45 +1,49 @@
 'use client';
 import { Card, CardDescription, CardHeader, CardTitle } from '../ui/card';
 import { toast } from 'sonner';
-import { Rocket, X } from 'lucide-react';
+import { X } from 'lucide-react';
 import { Button } from '../ui/button';
 import Timer from '../timer';
-import { useTask } from '../active-call/context';
 import { useTransition } from 'react';
+import { Task } from 'twilio-taskrouter';
 
-const TaskWrapup = () => {
-	const { task } = useTask();
+type Props = {
+	task: Task;
+	timer: {
+		seconds: number;
+		minutes: number;
+		hours: number;
+	};
+};
+
+const TaskWrapup = ({ task, timer }: Props) => {
 	const [isPending, startTransition] = useTransition();
-	return (
-		<form>
-			<Card className='w-[356px]'>
-				<CardHeader className='flex-row items-center p-3 gap-12 border-b'>
-					<CardTitle>
-						<Rocket className='h-3.5 w-3.5 inline-block mr-1.5 text-yellow-400' />
-						<span className='text-sm font-normal'>hey</span>
-					</CardTitle>
 
-					<CardDescription>
-						{task?.dateUpdated && <Timer startTime={task?.dateUpdated} />}
-						Incoming call
-						<Button
-							variant='ghost'
-							size='icon'
-							className='p-0 w-9 h-9'
-							type='submit'
-							onClick={() => {
-								startTransition(async () => {
-									await task?.complete('completed');
-									toast.dismiss(task?.sid);
-								});
-							}}
-						>
-							<X className='h-3.5 w-3.5 inline-block text-gray-400 cursor-pointer' />
-						</Button>
-					</CardDescription>
-				</CardHeader>
-			</Card>
-		</form>
+	return (
+		<Card>
+			<CardHeader className='flex-row items-center p-3 gap-12 border-b'>
+				<CardTitle className='text-lg'>Wrapping</CardTitle>
+
+				<CardDescription>
+					<Timer timer={timer} />
+
+					<Button
+						variant='ghost'
+						size='icon'
+						className='p-0 w-9 h-9'
+						type='submit'
+						onClick={() => {
+							startTransition(async () => {
+								await task?.complete('completed');
+								toast.dismiss(task?.sid);
+							});
+						}}
+					>
+						<X className='h-3.5 w-3.5 inline-block text-gray-400 cursor-pointer' />
+					</Button>
+				</CardDescription>
+			</CardHeader>
+		</Card>
 	);
 };
 

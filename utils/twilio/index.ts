764423564986c7d388e89dc263af18777d156a66
Nick Black <nickblack@VELO-V201849.local>
2024-09-06 08:33:25 -0500

    created reusable sync client

--- utils/twilio/index.ts
+++ utils/twilio/index.ts
@@ -1,7 +1,17 @@
 'use server';
 import { Twilio } from 'twilio';
+import { SyncClient } from 'twilio-sync';
+import { cookies } from 'next/headers';
 
 export const createClient = async () =>
 	await new Twilio(process.env.TWILIO_API_KEY_SID, process.env.TWILIO_API_KEY_SECRET, {
 		accountSid: process.env.TWILIO_ACCOUNT_SID,
 	});
+
+export const createSyncClient = async () => {
+	const token = cookies().get('twilio_token')?.value;
+	console.log('TOKEN', token);
+	if (!token) throw new Error('No token found');
+
+	return await new SyncClient(token);
+};

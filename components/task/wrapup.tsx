e824f22d6f7726e1b108b98aa7c75384e28a9039
Nick Black <nickblack@VELO-V201849.local>
2024-09-06 08:39:01 -0500

    added complete task mutation

--- components/task/wrapup.tsx
+++ components/task/wrapup.tsx
@@ -1,11 +1,11 @@
 'use client';
-import { Card, CardDescription, CardHeader, CardTitle } from '../ui/card';
-import { toast } from 'sonner';
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '../ui/card';
 import { X } from 'lucide-react';
 import { Button } from '../ui/button';
 import Timer from '../timer';
-import { useTransition } from 'react';
 import { Task } from 'twilio-taskrouter';
+import { useMutation } from '@tanstack/react-query';
+import { PopoverClose } from '@radix-ui/react-popover';
 
 type Props = {
 	task: Task;
@@ -17,7 +17,12 @@ type Props = {
 };
 
 const TaskWrapup = ({ task, timer }: Props) => {
-	const [isPending, startTransition] = useTransition();
+	const completeTaskMutation = useMutation({
+		mutationKey: ['completeTask'],
+		mutationFn: async () => {
+			await task?.complete('completed');
+		},
+	});
 
 	return (
 		<Card>
@@ -27,22 +32,21 @@ const TaskWrapup = ({ task, timer }: Props) => {
 				<CardDescription>
 					<Timer timer={timer} />
 
-					<Button
-						variant='ghost'
-						size='icon'
-						className='p-0 w-9 h-9'
-						type='submit'
-						onClick={() => {
-							startTransition(async () => {
-								await task?.complete('completed');
-								toast.dismiss(task?.sid);
-							});
-						}}
-					>
-						<X className='h-3.5 w-3.5 inline-block text-gray-400 cursor-pointer' />
-					</Button>
+					<PopoverClose asChild>
+						<Button
+							variant='ghost'
+							size='icon'
+							className='p-0 w-9 h-9'
+						>
+							<X className='h-3.5 w-3.5 inline-block text-gray-400 cursor-pointer' />
+						</Button>
+					</PopoverClose>
 				</CardDescription>
 			</CardHeader>
+
+			<CardFooter>
+				<Button onClick={() => completeTaskMutation.mutate()}>Complete</Button>
+			</CardFooter>
 		</Card>
 	);
 };

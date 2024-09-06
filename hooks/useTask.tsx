b71fcb02c344cec93a73443179206cb792347d84
Nick Black <nickblack@VELO-V201849.local>
2024-09-06 08:36:15 -0500

    adding default functions to useHook

--- hooks/useTask.tsx
+++ hooks/useTask.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { updateConferenceAction } from '@/components/active-call/actions';
 import { useDevice } from '@/providers/device-provider';
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 import { Task } from 'twilio-taskrouter';
 
 export type ConferenceParticpant = Record<string, Record<string, string>>;
@@ -15,7 +15,6 @@ export type ConferenceAttributes = {
 const useTask = (defaultTask: Task) => {
 	const { activeCall } = useDevice();
 	const searchParams = new URLSearchParams();
-	const [task, setTask] = useState<Task>(defaultTask);
 	const [attributes, setAttributes] = useState<Record<string, any>>(defaultTask.attributes);
 	const [conference, setConference] = useState<ConferenceAttributes>(defaultTask.attributes.conference);
 	searchParams.set('contactId', defaultTask.attributes.contactId);
@@ -37,57 +36,55 @@ const useTask = (defaultTask: Task) => {
 		},
 	};
 
-	useEffect(() => {
-		if (!defaultTask) return;
+	const {
+		data: task,
+		isLoading,
+		error,
+	} = useQuery({
+		queryKey: ['task'],
+		queryFn: () => defaultTask.fetchLatestVersion(),
+	});
 
-		setTask(defaultTask);
-		setAttributes(defaultTask.attributes);
-		setConference(defaultTask.attributes.conference ?? conferenceStub);
+	console.log(error, isLoading, task);
 
-		if (task.attributes.conference) {
-			setTimeout(() => {
-				task.fetchLatestVersion().then((task) => {
-					setTask(task);
-					setAttributes(task.attributes);
-					setConference(
-						task.attributes.conference
-							? {
-									...conferenceStub.conference,
-									...task.attributes.conference,
-									sid: task.attributes.conference.sid,
-									participants: {
-										worker: {
-											...conferenceStub.conference.participants.worker,
-											sid: task.attributes.conference.participants.worker.sid,
-										},
-										customer: {
-											...conferenceStub.conference.participants.customer,
-											sid: task.attributes.conference.participants.customer.sid,
-										},
-									},
-							  }
-							: conferenceStub
-					);
-				});
-			}, 1000);
-		}
-	}, [defaultTask]);
+	if (!isLoading) {
+		// setTask(taskData);
+		setAttributes(task?.attributes ?? {});
+		setConference(
+			task?.attributes.conference
+				? {
+						...conferenceStub.conference,
+						...task?.attributes.conference,
+						sid: task?.attributes.conference.sid,
+						participants: {
+							worker: {
+								...conferenceStub.conference.participants.worker,
+								sid: task?.attributes.conference.participants.worker.sid,
+							},
+							customer: {
+								...conferenceStub.conference.participants.customer,
+								sid: task?.attributes.conference.participants.customer.sid,
+							},
+						},
+				  }
+				: conferenceStub
+		);
+		const endConference = async () => {
+			activeCall?.disconnect();
+			await updateConferenceAction(task?.attributes.conference.sid, {
+				status: 'completed',
+			});
+			await task?.wrapUp({ reason: 'Call ended' });
+		};
 
-	const endConference = async () => {
-		activeCall?.disconnect();
-		await updateConferenceAction(task?.attributes.conference.sid, {
-			status: 'completed',
-		});
-		await task?.wrapUp({ reason: 'Call ended' });
-	};
-
-	return {
-		task,
-		attributes,
-		conference,
-		endConference,
-		searchParams,
-	};
+		return {
+			task,
+			attributes,
+			conference,
+			endConference,
+			searchParams,
+		};
+	}
 };
 
 export default useTask;

747e46036240356a6855a484ee2ad90f980705d3
Nick Black <nickblack@VELO-V201849.local>
2024-09-10 08:44:17 -0500

    added a redirect if no user is found

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -33,15 +33,14 @@ export async function updateSession(request: NextRequest) {
 		data: { user },
 	} = await supabase.auth.getUser();
 
-	// console.log(request.nextUrl.basePath);
-
-	// if ((!user && !request.nextUrl.pathname.startsWith('/login')) || !request.nextUrl.pathname.startsWith('/auth')) {
-	// 	// no user, potentially respond by redirecting the user to the login page
-	// 	const url = request.nextUrl.clone();
-	// 	url.pathname = '/login';
-	// 	return NextResponse.redirect(url);
-	// }
-
+	if (!user) {
+		if (!request.nextUrl.pathname.includes('login') && !request.nextUrl.pathname.includes('auth')) {
+			// no user, potentially respond by redirecting the user to the login page
+			const url = request.nextUrl.clone();
+			url.pathname = '/login';
+			return NextResponse.redirect(url);
+		}
+	}
 	// IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
 	// creating a new response object with NextResponse.next() make sure to:
 	// 1. Pass the request in it, like so:

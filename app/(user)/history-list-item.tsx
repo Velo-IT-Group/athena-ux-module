c85171974ff7c33883296e254343db3b004a3b9e
Nick Black <nickblack@VELO-V201849.local>
2024-09-13 14:52:57 -0500

    super clean history

--- app/(user)/history-list-item.tsx
+++ app/(user)/history-list-item.tsx
@@ -1,30 +1,52 @@
 'use client';
 import { CommandItem } from '@/components/ui/command';
+import { getContact } from '@/lib/manage/read';
+import { parsePhoneNumber } from '@/lib/utils';
+import { addSeconds } from '@/utils/date';
+import { useQuery } from '@tanstack/react-query';
 import { PhoneIncoming, PhoneOutgoing } from 'lucide-react';
 import React from 'react';
-import type { CallInstance } from 'twilio/lib/rest/api/v2010/account/call';
 
 type Props = {
-	call: CallInstance;
+	conversation: Conversation;
 };
 
-const HistoryListItem = ({ call }: Props) => {
+const HistoryListItem = ({ conversation }: Props) => {
+	const startTime = new Date(conversation.date);
+	const endTime = addSeconds(startTime, conversation?.talk_time ?? 0);
+	const { data } = useQuery({
+		queryKey: ['contact', conversation.contact_id],
+		// @ts-ignore
+		queryFn: () => getContact(conversation.contact_id, { fields: ['id', 'firstName', 'lastName', 'company/name'] }),
+	});
+
+	const name = data ? `${data?.firstName} ${data?.lastName ?? ''}` : undefined;
+	const value = `${Object.values(data ? { ...data, company: data.company?.name } : {}).toString()} ${
+		conversation.date
+	}`;
+
 	return (
 		<CommandItem
-			key={call.sid}
-			value={call.sid}
-			onSelect={(currentValue) => {
-				console.log(currentValue);
-			}}
+			key={conversation.id}
+			value={value}
 		>
-			{call.direction === 'outbound' ? (
-				<PhoneOutgoing className='mr-1.5 text-red-500' />
+			{conversation.direction === 'outbound' ? (
+				<PhoneOutgoing className='mr-3 text-red-500' />
 			) : (
-				<PhoneIncoming className='mr-1.5 text-green-500' />
+				<PhoneIncoming className='mr-3 text-green-500' />
 			)}
-			<span className='text-muted-foreground'>
-				{call.fromFormatted} ({Intl.DateTimeFormat('en-US', { timeStyle: 'short' }).format(new Date(call.dateCreated))})
-			</span>
+			<div>
+				{name && name}
+				{name && ' • '}
+				{parsePhoneNumber(conversation.phone_number ?? '').formattedNumber}
+				<div>
+					<span className='text-muted-foreground text-xs'>
+						{Intl.DateTimeFormat('en-US', { timeStyle: 'short' }).format(startTime)}
+						{' - '}
+						{Intl.DateTimeFormat('en-US', { timeStyle: 'short' }).format(endTime)}
+					</span>
+				</div>
+			</div>
 		</CommandItem>
 	);
 };

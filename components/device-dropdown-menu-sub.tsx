d22c2c07568091c155658fe927dcb239181f304b
Nick Black <nickblack@VELO-V201849.local>
2024-09-13 18:34:11 -0500

    added device selector

--- components/device-dropdown-menu-sub.tsx
+++ components/device-dropdown-menu-sub.tsx
@@ -4,14 +4,60 @@ import { Check, Headset } from 'lucide-react';
 import { Command, CommandGroup, CommandItem, CommandList, CommandSeparator } from './ui/command';
 import { cn } from '@/lib/utils';
 import { useDevice } from '@/providers/device-provider';
-import { selectInputDevice, selectOutputDevice } from '@/utils/twilio/device';
+import { selectInputDevice, selectOutputDevice, setSelectedDevice } from '@/utils/twilio/device';
+import { useEffect, useState } from 'react';
+import { toast } from 'sonner';
 
 type Props = {};
 
 const DeviceDropdownMenuSub = (props: Props) => {
+	const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
+	const [selectedInput, setSelectedInput] = useState<MediaDeviceInfo>();
+	const [selectedOutput, setSelectedOutput] = useState<MediaDeviceInfo>();
 	const { device } = useDevice();
 
 	if (!device?.audio) return;
+
+	function refreshDevices() {
+		navigator.mediaDevices.enumerateDevices().then((foundDevices) => {
+			setDevices(foundDevices.filter((device) => !device.label.includes('Default')));
+			const defaultOutput = foundDevices.find(
+				(device) => device.label.includes('Default') && device.kind === 'audiooutput'
+			);
+			const defaultInput = foundDevices.find(
+				(device) => device.label.includes('Default') && device.kind === 'audioinput'
+			);
+			setSelectedOutput(
+				foundDevices.find((device) => device.groupId === defaultOutput?.groupId && !device.label.includes('Default'))
+			);
+			setSelectedInput(
+				foundDevices.find((device) => device.groupId === defaultInput?.groupId && !device.label.includes('Default'))
+			);
+		});
+	}
+
+	useEffect(() => {
+		if (!window || !navigator) return;
+		refreshDevices();
+		navigator.mediaDevices.addEventListener('devicechange', refreshDevices);
+
+		return function cleanup() {
+			navigator.mediaDevices.removeEventListener('devicechange', refreshDevices);
+		};
+	}, [window, navigator]);
+
+	const selectedDevice = (selectedDevice: MediaDeviceInfo) => {
+		try {
+			setSelectedDevice(selectedDevice, device);
+			setSelectedDevice;
+
+			toast.success(`Set ${selectedDevice.label} as your audio device.`);
+		} catch (e) {
+			toast.error(`There was an error attempting to set ${selectedDevice.label} as your audio device.`);
+		}
+	};
+
+	const isSameDevice = selectedInput?.label === selectedOutput?.label;
 
 	return (
 		<DropdownMenuSub>
@@ -20,58 +66,65 @@ const DeviceDropdownMenuSub = (props: Props) => {
 					<CommandList>
 						<CommandGroup heading='Speaker'>
 							<CommandList>
-								{/* setAvailableInputDevices(Array.from(device.audio?.availableInputDevices?.values() ?? []));
-								setAvailableOutputDevices(Array.from(device.audio?.availableOutputDevices?.values() ?? [])); */}
-								{Array.from(device?.audio.availableOutputDevices?.values()).map((value) => {
-									const mediaDevice = value as MediaDeviceInfo;
-									return (
-										<CommandItem
-											key={mediaDevice.deviceId}
-											value={mediaDevice.groupId}
-											onClick={async () => {
-												await selectOutputDevice(mediaDevice.deviceId, device);
-											}}
-										>
-											<Check
-												className={cn('mr-2 h-3.5 w-3.5', mediaDevice?.groupId === '' ? 'opacity-100' : 'opacity-0')}
-											/>
-											{mediaDevice.label}
-										</CommandItem>
-									);
-								})}
+								{devices
+									.filter((device) => device.kind === 'audiooutput')
+									.map((value) => {
+										const mediaDevice = value as MediaDeviceInfo;
+										return (
+											<CommandItem
+												key={mediaDevice.deviceId}
+												value={mediaDevice.groupId}
+												onClick={() => selectedDevice(mediaDevice)}
+											>
+												<Check
+													className={cn(
+														'mr-2 h-3.5 w-3.5',
+														selectedOutput?.groupId === mediaDevice.groupId ? 'opacity-100' : 'opacity-0'
+													)}
+												/>
+												{mediaDevice.label}
+											</CommandItem>
+										);
+									})}
 							</CommandList>
 						</CommandGroup>
 
 						<CommandSeparator />
 
 						<CommandGroup heading='Microphone'>
-							{Array.from(device?.audio.availableInputDevices?.values()).map((value) => {
-								const mediaDevice = value as MediaDeviceInfo;
-								return (
-									<CommandItem
-										key={mediaDevice.deviceId}
-										value={mediaDevice.groupId}
-										onClick={async () => {
-											await selectInputDevice(mediaDevice.deviceId, device);
-										}}
-									>
-										<Check
-											className={cn(
-												'mr-2 h-3.5 w-3.5',
-												mediaDevice?.groupId === device.audio?.inputDevice?.groupId ? 'opacity-100' : 'opacity-0'
-											)}
-										/>
-										{mediaDevice.label}
-									</CommandItem>
-								);
-							})}
+							{devices
+								.filter((device) => device.kind === 'audioinput')
+								.map((value) => {
+									const mediaDevice = value as MediaDeviceInfo;
+									return (
+										<CommandItem
+											key={mediaDevice.deviceId}
+											value={mediaDevice.groupId}
+											onClick={() => selectedDevice(mediaDevice)}
+										>
+											<Check
+												className={cn(
+													'mr-2 h-3.5 w-3.5',
+													selectedInput?.groupId === mediaDevice.groupId ? 'opacity-100' : 'opacity-0'
+												)}
+											/>
+											{mediaDevice.label}
+										</CommandItem>
+									);
+								})}
 						</CommandGroup>
 					</CommandList>
 				</Command>
 			</DropdownMenuSubContent>
 
 			<DropdownMenuSubTrigger>
-				<Headset className='mr-1.5' /> {device.audio.inputDevice?.label}
+				{isSameDevice ? (
+					<>
+						<Headset className='mr-1.5' /> {selectedInput?.label ?? 'Select Input Device'}
+					</>
+				) : (
+					<></>
+				)}
 			</DropdownMenuSubTrigger>
 		</DropdownMenuSub>
 	);

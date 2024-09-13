ef8dead92b1744f515831d099d758c51f9741690
nickblack26 <nicholas.black98@icloud.com>
2024-09-13 07:58:36 -0500

    removed local

--- components/active-call/index.tsx
+++ components/active-call/index.tsx
@@ -3,30 +3,35 @@ import { Card } from '@/components/ui/card';
 import ActiveCallHeader from './header';
 import ActiveCallFooter from './footer';
 import ActiveCallParticipants from './participants';
-import { Task, Workspace } from 'twilio-taskrouter';
+import { Task } from 'twilio-taskrouter';
 import useTask from '@/hooks/useTask';
 import useConference from '@/hooks/useConference';
 import { useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 type Props = {
 	task: Task;
 };
 
 export function ActiveCall({ task }: Props) {
+	// const { data } = useQuery({
+	// 	queryKey: ['queryParticipants', task.attributes.conference.sid],
+	// 	queryFn: () => client.conferences(task.attributes.conference.sid).fetch(),
+	// });
+
+	// console.log(data);
+
 	const { transferTask, conference } = useTask(task);
-	const { addConferenceParticipantMutation, conferenceParticipants, endConference, setConferenceParticipants } =
+	const { addConferenceParticipantMutation, conferenceParticipants, endConference, updateConferenceParticipantsState } =
 		useConference({
 			conference,
 			task,
 		});
 
 	useEffect(() => {
-		// if (!conference) return;
-		const part = conference.participants;
-		setConferenceParticipants((prev) => {
-			return { ...prev, ...part };
-		});
-	}, [conference.participants]);
+		if (!conference) return;
+		updateConferenceParticipantsState(conference.participants);
+	}, [conference]);
 
 	// const confP = { ...conference.participants, ...conferenceParticipants };
 

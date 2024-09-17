59d238789c850643a85968e3a91b932b7fd0c31c
nickblack26 <nicholas.black98@icloud.com>
2024-09-17 07:42:40 -0500

    added workspace to return value

--- providers/twilio-provider.tsx
+++ providers/twilio-provider.tsx
@@ -1,15 +1,18 @@
 'use client';
 import { useContext, useState, Dispatch, createContext, useRef } from 'react';
+import { Workspace } from 'twilio-taskrouter';
 interface TwilioProviderProps {
 	currentWorkspace: string | undefined;
 	setCurrentWorkspace: Dispatch<React.SetStateAction<string | undefined>>;
 	token: string;
+	workspace: Workspace | undefined;
 }
 
 const initialValues: TwilioProviderProps = {
 	currentWorkspace: '',
 	setCurrentWorkspace: () => undefined,
 	token: '',
+	workspace: undefined,
 };
 
 type WithChildProps = {
@@ -25,11 +28,13 @@ const { Provider } = context;
 export const TwilioProvider = ({ authToken, workspaceSid, children }: WithChildProps) => {
 	const tokenRef = useRef(authToken);
 	const [currentWorkspace, setCurrentWorkspace] = useState<string | undefined>(workspaceSid);
+	const workspace = new Workspace(authToken, {}, currentWorkspace);
 
 	const values = {
 		currentWorkspace,
 		setCurrentWorkspace,
 		token: authToken,
+		workspace,
 	};
 
 	return <Provider value={values}>{children}</Provider>;

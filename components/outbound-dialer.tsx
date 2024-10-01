0b53e5a747743c98fcb8d66ebc045ba1a6eae351
Nick Black <nickblack@VELO-V201849.local>
2024-10-01 09:01:02 -0500

    adding toolbar to test headset device

--- components/outbound-dialer.tsx
+++ components/outbound-dialer.tsx
@@ -1,9 +1,14 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import OutboundDialerContent from './outbound-dialer-content';
 import { useWorker } from '@/providers/worker-provider';
 import { parsePhoneNumber } from '@/lib/utils';
 import { toast } from 'sonner';
+import { Separator } from './ui/separator';
+import { Button } from './ui/button';
+import { BellRing, FlaskConical, Mic, Volume2 } from 'lucide-react';
+import { Progress } from './ui/progress';
+import { useDevice } from '@/providers/device-provider';
 
 type Props = {};
 
@@ -14,34 +19,85 @@ const numbers = {
 };
 
 const OutboundDialer = (props: Props) => {
+	const [isRinging, setIsRinging] = useState(false);
 	const { worker } = useWorker();
+	const { currentCallControl, testDevice } = useDevice();
 
 	return (
-		<OutboundDialerContent
-			numbers={[]}
-			onSubmit={(data) => {
-				try {
-					const to = data.get('To') as string;
-					const splitNumber: string[] = to.split(' ');
-					const areaCode = splitNumber?.[1];
-					worker?.createTask(
-						parsePhoneNumber(to, 'US', 'E.164').formattedNumber ?? '',
-						// @ts-ignore
-						(numbers[areaCode] as string) ?? process.env.NEXT_PUBLIC_TWILIO_PHONE_NUMBER,
-						process.env.NEXT_PUBLIC_TWILIO_WORKFLOW_SID!,
-						process.env.NEXT_PUBLIC_TWILIO_TASK_QUEUE_SID!,
-						{
-							attributes: {
-								direction: 'outbound',
-							},
-							taskChannelUniqueName: 'voice',
-						}
-					);
-				} catch (error) {
-					toast.error(JSON.stringify(error, null, 2));
-				}
-			}}
-		/>
+		<div className='space-y-1.5'>
+			<OutboundDialerContent
+				numbers={[]}
+				onSubmit={(data) => {
+					try {
+						const to = data.get('To') as string;
+						const splitNumber: string[] = to.split(' ');
+						const areaCode = splitNumber?.[1];
+						worker?.createTask(
+							parsePhoneNumber(to, 'US', 'E.164').formattedNumber ?? '',
+							// @ts-ignore
+							(numbers[areaCode] as string) ?? process.env.NEXT_PUBLIC_TWILIO_PHONE_NUMBER,
+							process.env.NEXT_PUBLIC_TWILIO_WORKFLOW_SID!,
+							process.env.NEXT_PUBLIC_TWILIO_TASK_QUEUE_SID!,
+							{
+								attributes: {
+									direction: 'outbound',
+								},
+								taskChannelUniqueName: 'voice',
+							}
+						);
+					} catch (error) {
+						toast.error(JSON.stringify(error, null, 2));
+					}
+				}}
+			/>
+
+			<Separator />
+
+			<div className='flex items-center gap-1.5'>
+				<Button
+					variant='outline'
+					size='icon'
+					className='shrink-0'
+					onClick={() => {
+						currentCallControl?.ring(!isRinging);
+						setIsRinging((prev) => !prev);
+					}}
+				>
+					<BellRing />
+				</Button>
+
+				<Button
+					variant='outline'
+					size='icon'
+					className='shrink-0'
+					onClick={() => {
+						// currentCallControl?.ring(!isRinging);
+						testDevice();
+						// setIsRinging((prev) => !prev);
+					}}
+				>
+					<FlaskConical />
+				</Button>
+
+				<Button
+					variant='outline'
+					size='icon'
+					className='shrink-0'
+				>
+					<Mic />
+				</Button>
+
+				<Progress className='shrink grow-0' />
+
+				<Button
+					variant='outline'
+					size='icon'
+					className='shrink-0'
+				>
+					<Volume2 />
+				</Button>
+			</div>
+		</div>
 	);
 };
 

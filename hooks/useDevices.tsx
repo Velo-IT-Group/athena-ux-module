c0623d2c5a135b46b5fa8d6036dbba08682588b6
Nick Black <nickblack@VELO-V201849.local>
2024-10-02 09:03:55 -0500

    filtering out default devices

--- hooks/useDevices.tsx
+++ hooks/useDevices.tsx
@@ -7,20 +7,6 @@ export default function useDevices() {
 
 	useEffect(() => {
 		if (!window || !navigator) return;
-		navigator.mediaDevices
-			.getUserMedia({ video: false, audio: true })
-			.then((stream) => {
-				// @ts-ignore
-				window.localStream = stream; // A
-				// @ts-ignore
-				window.localAudio.srcObject = stream; // B
-				// @ts-ignore
-				window.localAudio.autoplay = true; // C
-			})
-			.catch((err) => {
-				console.error(`you got an error: ${err}`);
-				toast.error(err);
-			});
 		const getDevices = () => navigator.mediaDevices.enumerateDevices().then((newDevices) => setDevices(newDevices));
 		navigator.mediaDevices.addEventListener('devicechange', getDevices);
 		getDevices();
@@ -30,14 +16,14 @@ export default function useDevices() {
 		};
 	}, [window, navigator]);
 
-	const audioInputDevices = devices.filter((device) => device.kind === 'audioinput');
-	const audioOutputDevices = devices.filter((device) => device.kind === 'audiooutput');
+	const audioInputDevices = devices.filter((device) => device.kind === 'audioinput') ?? [];
+	const audioOutputDevices = devices.filter((device) => device.kind === 'audiooutput') ?? [];
 
 	return {
-		audioInputDevices,
+		audioInputDevices: audioInputDevices.filter((d) => d.deviceId !== 'default'),
 		videoInputDevices: devices.filter((device) => device.kind === 'videoinput'),
-		audioOutputDevices,
-		defaultOutput: audioOutputDevices.find((d) => d.deviceId === 'Default'),
-		defaultInput: audioInputDevices.find((d) => d.deviceId === 'Default'),
+		audioOutputDevices: audioOutputDevices.filter((d) => d.deviceId !== 'default'),
+		defaultOutput: audioOutputDevices.find((d) => d.deviceId === 'default'),
+		defaultInput: audioInputDevices.find((d) => d.deviceId === 'default'),
 	};
 }

c1723ac234381c50ff5e271352ca0a3fbd1b208a
Nick Black <nickblack@VELO-V201849.local>
2024-10-02 14:08:58 -0500

    moved dialing mechanism to chilid

--- components/outbound-dialer.tsx
+++ components/outbound-dialer.tsx
@@ -1,109 +1,21 @@
 'use client';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import OutboundDialerContent from './outbound-dialer-content';
-import { useWorker } from '@/providers/worker-provider';
-import { parsePhoneNumber } from '@/lib/utils';
 import { toast } from 'sonner';
 import { Separator } from './ui/separator';
 import { Button } from './ui/button';
-import { BellRing, FlaskConical, Mic, Volume2 } from 'lucide-react';
-import { Progress } from './ui/progress';
+import { BellRing, FlaskConical } from 'lucide-react';
 import { useDevice } from '@/providers/device-provider';
-import useAudioTest from '@/hooks/useAudioTest';
-import { lookupPhoneNumber } from '@/lib/twilio/phoneNumbers';
-import { SignalType, webHidPairing } from '@gnaudio/jabra-js';
 
 type Props = {};
 
-const numbers = {
-	'214': '+12142148356',
-	'281': '+12817208356',
-	'337': '+13377067517',
-};
-
 const OutboundDialer = (props: Props) => {
-	const [inputDeviceId, setInputDeviceId] = useState('');
-	const [outputDeviceId, setOutputDeviceId] = useState('');
-	const previousInputDeviceIdRef = useRef('');
-	const {
-		error,
-		setError,
-		isRecording,
-		isAudioInputTestRunning,
-		isAudioOutputTestRunning,
-		playAudio,
-		playbackURI,
-		readAudioInput,
-		stopAudioTest,
-		inputLevel,
-		outputLevel,
-	} = useAudioTest();
-	const volumeLevel = isAudioOutputTestRunning ? outputLevel : inputLevel;
-
-	// stop test when not on AudioTest and there's an active test
-	useEffect(() => {
-		const newInputDeviceSelected = previousInputDeviceIdRef.current !== inputDeviceId;
-		previousInputDeviceIdRef.current = inputDeviceId;
-
-		// Restarts the test to continuously capture audio input
-		if (!error && (newInputDeviceSelected || (!isRecording && !isAudioInputTestRunning))) {
-			readAudioInput({ deviceId: inputDeviceId });
-		}
-
-		if (error) {
-			stopAudioTest();
-		}
-
-		return () => {
-			// stopAudioTest();
-		};
-	}, [
-		error,
-		inputDeviceId,
-		isRecording,
-		isAudioInputTestRunning,
-		readAudioInput,
-		stopAudioTest,
-		stopAudioTest,
-		isAudioInputTestRunning,
-		isAudioOutputTestRunning,
-	]);
-
 	const [isRinging, setIsRinging] = useState(false);
-	const { worker } = useWorker();
-	const { currentCallControl, testDevice, device } = useDevice();
+	const { currentCallControl, testDevice } = useDevice();
 
 	return (
 		<div className='space-y-1.5'>
-			<OutboundDialerContent
-				numbers={[]}
-				onSubmit={async (data) => {
-					try {
-						const to = data.get('To') as string;
-						const splitNumber: string[] = to.split(' ');
-						const areaCode = splitNumber?.[1];
-						const numberReturn = await lookupPhoneNumber(to);
-						worker?.createTask(
-							parsePhoneNumber(to, 'US', 'E.164').formattedNumber ?? '',
-							// @ts-ignore
-							(numbers[areaCode] as string) ?? process.env.NEXT_PUBLIC_TWILIO_PHONE_NUMBER,
-							process.env.NEXT_PUBLIC_TWILIO_WORKFLOW_SID!,
-							process.env.NEXT_PUBLIC_TWILIO_TASK_QUEUE_SID!,
-							{
-								attributes: {
-									direction: 'outbound',
-									name: numberReturn?.name,
-									companyId: numberReturn?.companyId,
-									contactId: numberReturn?.userId,
-								},
-								taskChannelUniqueName: 'voice',
-							}
-						);
-					} catch (error) {
-						toast.error(JSON.stringify(error, null, 2));
-					}
-				}}
-			/>
+			<OutboundDialerContent numbers={[]} />
 
 			<Separator />
 
@@ -114,9 +26,6 @@ const OutboundDialer = (props: Props) => {
 					className='shrink-0'
 					onClick={async () => {
 						try {
-							// await webHidPairing();
-							// currentCallControl?.takeCallLock();
-							console.log(currentCallControl);
 							currentCallControl?.ring(!isRinging);
 							setIsRinging((prev) => !prev);
 						} catch (error) {
@@ -133,37 +42,11 @@ const OutboundDialer = (props: Props) => {
 					size='icon'
 					className='shrink-0'
 					onClick={() => {
-						// currentCallControl?.ring(!isRinging);
 						testDevice();
-						// setIsRinging((prev) => !prev);
 					}}
 				>
 					<FlaskConical />
 				</Button>
-
-				{/* <Button
-					variant='outline'
-					size='icon'
-					className='shrink-0'
-				>
-					<Mic />
-				</Button> */}
-
-				<Progress
-					value={volumeLevel}
-					className='shrink grow-0'
-				/>
-
-				{/* <Button
-					variant='outline'
-					size='icon'
-					className='shrink-0'
-					onClick={() => {
-						device?.connect({ params: { To: '+19032962250' } });
-					}}
-				>
-					<Volume2 />
-				</Button> */}
 			</div>
 		</div>
 	);

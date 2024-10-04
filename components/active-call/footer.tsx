ff355e754dea39bfdbd61b460136cfa07db1cc83
Nick Black <nickblack@VELO-V201849.local>
2024-10-04 15:02:01 -0500

    passing handle submit function

--- components/active-call/footer.tsx
+++ components/active-call/footer.tsx
@@ -8,10 +8,11 @@ import { Popover, PopoverTrigger, PopoverContent } from '../ui/popover';
 import { useDevice } from '@/providers/device-provider';
 import WorkerSelector from '@/app/(user)/worker-selector';
 import { parsePhoneNumber } from '@/lib/utils';
-import OutboundDialerContent from '../outbound-dialer-content';
+import OutboundDialerContent, { outboundPhoneSchema } from '../outbound-dialer-content';
 import { useTaskContext } from './context';
 import { toast } from 'sonner';
 import { removeConferenceParticipant } from '@/lib/twilio/conference/helpers';
+import { z } from 'zod';
 
 const ActiveCallFooter = () => {
 	const {
@@ -26,6 +27,24 @@ const ActiveCallFooter = () => {
 		updateParticipants,
 	} = useTaskContext();
 	const { muted, setMuted } = useDevice();
+
+	async function onSubmit(values: z.infer<typeof outboundPhoneSchema>) {
+		// Do something with the form values.
+		// ✅ This will be type-safe and validated.
+		try {
+			const parsedNumber = parsePhoneNumber(values.To, 'US', 'E.164').formattedNumber ?? '';
+			const attributes = {
+				externalContact: parsePhoneNumber(values.To, 'US').formattedNumber,
+			};
+			addExternalParticipant?.mutate({
+				From: task?.attributes.to ?? task?.attributes.from,
+				To: parsedNumber,
+				attributes,
+			});
+		} catch (error) {
+			toast.error(JSON.stringify(error, null, 2));
+		}
+	}
 
 	return (
 		<CardFooter className='p-3 border-t space-x-1.5 justify-between'>
@@ -113,20 +132,11 @@ const ActiveCallFooter = () => {
 					<PopoverContent
 						side='bottom'
 						align='start'
+						forceMount
 					>
 						<OutboundDialerContent
 							numbers={[]}
-							onSubmit={(data) => {
-								const parsedNumber = parsePhoneNumber(data.get('To') as string, 'US', 'E.164').formattedNumber ?? '';
-								const attributes = {
-									externalContact: parsePhoneNumber(data.get('To') as string, 'US').formattedNumber,
-								};
-								addExternalParticipant?.mutate({
-									From: task?.attributes.to ?? task?.attributes.from,
-									To: parsedNumber,
-									attributes,
-								});
-							}}
+							onSubmit={onSubmit}
 						/>
 					</PopoverContent>
 				</Popover>

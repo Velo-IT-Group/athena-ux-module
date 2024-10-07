c6d2138a4352e7fafa78b74f7b6357754a9eea01
Nick Black <nickblack@VELO-V201849.local>
2024-10-07 09:01:54 -0500

    added inital links to sidebar

--- config/links.ts
+++ config/links.ts
@@ -1,15 +1,34 @@
 import { NavItem, NavItemWithChildren } from '@/types/nav';
+import {  CircleDotDashed, Inbox } from 'lucide-react';
 
 export interface LinksConfig {
 	modules: NavItem[];
 	mainNav: NavItem[];
-	sidebarNav: NavItem[];
+	sidebarNav: NavItemWithChildren[];
 	settingsNav: NavItemWithChildren[];
 }
 
 export const linksConfig: LinksConfig = {
 	modules: [],
 	mainNav: [],
-	sidebarNav: [],
+	sidebarNav: [
+		{
+			title: '',
+			items: [
+				{
+					title: 'Inbox',
+					icon: Inbox, 
+					href: '/inbox',
+					items: []
+				},
+				{
+					title: 'My issues',
+					icon: CircleDotDashed, 
+					href: '/my-issues',
+					items: []
+				},
+			]
+		}
+	],
 	settingsNav: [],
 };

34b24cf94e76d634cb636f765c9d76652525ea3b
Nick Black <nickblack@VELO-V201849.local>
2024-10-18 09:38:48 -0500

    added a quick and dirty way to clear query params

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,26 @@
+'use client';
+import { useRouter, useSearchParams } from 'next/navigation';
 import React from 'react';
+import { Button } from './ui/button';
+import { X } from 'lucide-react';
 
 const Navbar = () => {
+	const params = useSearchParams();
+	const { push } = useRouter();
+
 	return (
 		<nav className='flex items-center gap-1.5 px-3 py-0.5 h-12 border-b'>
 			<h2 className='text-sm font-medium tracking-tight'>Dashboard</h2>
+			{Array.from(params.entries()).length > 0 && (
+				<Button
+					variant='ghost'
+					size='sm'
+					onClick={() => push('/')}
+				>
+					<span>Clear</span>
+					<X className='ml-1.5' />
+				</Button>
+			)}
 		</nav>
 	);
 };

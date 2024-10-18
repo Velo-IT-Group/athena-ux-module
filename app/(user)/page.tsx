4ebce4b766f5c4ef6095353056e0bd600b569caf
Nick Black <nickblack@VELO-V201849.local>
2024-10-18 08:42:46 -0500

    await search params

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -2,19 +2,20 @@ import ConversationContactDetail from './conversation-contact-detail';
 import ConversationDetails from './active-conversation';
 import { createClient } from '@/utils/supabase/server';
 
-export default async function Page({
-	searchParams,
-}: {
-	searchParams: { [key: string]: string | string[] | undefined };
-}) {
-	const supabase = await createClient();
-	const companyId = searchParams.companyId ? parseInt(searchParams.companyId as string) : undefined;
-	const contactId = searchParams.contactId ? parseInt(searchParams.contactId as string) : undefined;
-	const {
+export default async function Page(
+    props: {
+        searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+    }
+) {
+    const searchParams = await props.searchParams;
+    const supabase = await createClient();
+    const companyId = searchParams.companyId ? parseInt(searchParams.companyId as string) : undefined;
+    const contactId = searchParams.contactId ? parseInt(searchParams.contactId as string) : undefined;
+    const {
 		data: { user },
 	} = await supabase.auth.getUser();
 
-	return (
+    return (
 		<main className='grid grid-cols-[1fr_3fr] w-full gap-3'>
 			<ConversationContactDetail contactId={contactId ?? user?.user_metadata?.contactId} />
 

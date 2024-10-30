ac04f2eeff5b41021bdfa2b05d8e3574fc8b0cfe
Nick Black <nickblack@VELO-V201849.local>
2024-10-30 14:36:34 -0500

    defaulting company to velo

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -2,26 +2,22 @@ import ConversationContactDetail from './conversation-contact-detail';
 import ConversationDetails from './active-conversation';
 import { createClient } from '@/utils/supabase/server';
 
-export default async function Page(
-    props: {
-        searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-    }
-) {
-    const searchParams = await props.searchParams;
-    const supabase = await createClient();
-    const companyId = searchParams.companyId ? parseInt(searchParams.companyId as string) : undefined;
-    const contactId = searchParams.contactId ? parseInt(searchParams.contactId as string) : undefined;
-    const {
+export default async function Page(props: { searchParams: Promise<{ [key: string]: string | string[] | undefined }> }) {
+	const searchParams = await props.searchParams;
+	const supabase = await createClient();
+	const companyId = searchParams.companyId ? parseInt(searchParams.companyId as string) : undefined;
+	const contactId = searchParams.contactId ? parseInt(searchParams.contactId as string) : undefined;
+	const {
 		data: { user },
 	} = await supabase.auth.getUser();
 
-    return (
+	return (
 		<main className='grid grid-cols-[1fr_3fr] w-full gap-3'>
 			<ConversationContactDetail contactId={contactId ?? user?.user_metadata?.contactId} />
 
 			<ConversationDetails
 				contactId={contactId}
-				companyId={companyId}
+				companyId={companyId ?? 250}
 				className='p-6'
 			/>
 		</main>

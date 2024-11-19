2ed31821f4acf029f03631c70e98fdf3b87c62aa
Nick Black <nickblack@VELO-V201849.local>
2024-11-19 13:27:29 -0600

    added title

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,28 +1,37 @@
 'use client';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import React from 'react';
 import { Button } from './ui/button';
 import { X } from 'lucide-react';
 import { SidebarTrigger } from './ui/sidebar';
 
-const Navbar = () => {
+type Props = {
+	title?: string;
+	children?: React.ReactNode;
+};
+
+const Navbar = ({ title = 'Dashboard', children }: Props) => {
 	const params = useSearchParams();
 	const { push } = useRouter();
+	const pathname = usePathname();
 
 	return (
-		<nav className='flex items-center gap-1.5 px-3 py-0.5 h-12 border-b'>
+		<nav className="flex items-center gap-1.5 px-3 py-0.5 h-12 border-b">
 			<SidebarTrigger />
-			<h2 className='text-sm font-medium tracking-tight'>Dashboard</h2>
+
+			<h2 className="text-sm font-medium tracking-tight">{title}</h2>
+
 			{Array.from(params.entries()).length > 0 && (
 				<Button
-					variant='ghost'
-					size='sm'
-					onClick={() => push('/')}
-				>
+					variant="ghost"
+					size="sm"
+					onClick={() => push(pathname)}>
 					<span>Clear</span>
-					<X className='ml-1.5' />
+					<X className="ml-1.5" />
 				</Button>
 			)}
+
+			{children}
 		</nav>
 	);
 };

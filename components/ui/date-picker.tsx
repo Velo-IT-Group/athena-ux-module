5948261662c39570b58d41710081ddd143e476cb
Nick Black <nickblack@VELO-V201849.local>
2024-11-19 13:25:58 -0600

    added date picker options

--- components/ui/date-picker.tsx
+++ components/ui/date-picker.tsx
@@ -1,52 +1,233 @@
 'use client';
 
 import * as React from 'react';
-import { format } from 'date-fns';
+import {
+	endOfMonth,
+	endOfQuarter,
+	format,
+	startOfMonth,
+	startOfQuarter,
+	subDays,
+	subWeeks,
+} from 'date-fns';
+import { Calendar as CalendarIcon } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { Calendar } from '@/components/ui/calendar';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { CalendarIcon } from 'lucide-react';
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '@/components/ui/popover';
 
-type Props = {
-	date?: Date | null;
-	setDate?: (date: Date) => void;
-	name?: string;
-};
+export function DatePickerDemo() {
+	const [date, setDate] = React.useState<Date>();
 
-const DatePicker = ({ date, setDate, name }: Props) => {
 	return (
-		<>
+		<Popover>
+			<PopoverTrigger asChild>
+				<Button
+					variant={'outline'}
+					className={cn(
+						'w-[280px] justify-start text-left font-normal',
+						!date && 'text-muted-foreground'
+					)}>
+					<CalendarIcon className="mr-1.5 h-3 w-3" />
+					{date ? format(date, 'PPP') : <span>Pick a date</span>}
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent className="w-auto p-0">
+				<Calendar
+					mode="single"
+					selected={date}
+					onSelect={setDate}
+					initialFocus
+				/>
+			</PopoverContent>
+		</Popover>
+	);
+}
+
+import { DateRange } from 'react-day-picker';
+import { usePathname, useRouter } from 'next/navigation';
+
+export function DatePickerWithRange({
+	className,
+}: React.HTMLAttributes<HTMLDivElement>) {
+	const newDate = new Date();
+	const [date, setDate] = React.useState<DateRange | undefined>({
+		from: subDays(newDate, 20),
+		to: newDate,
+	});
+
+	return (
+		<div className={cn('grid gap-1.5', className)}>
 			<Popover>
 				<PopoverTrigger asChild>
 					<Button
+						id="date"
 						variant={'outline'}
-						size={'sm'}
-						className={cn('justify-start text-left font-normal text-xs', !date && 'text-muted-foreground')}
-					>
-						<CalendarIcon className='mr-1.5' />
-						{date ? format(date, 'PPP') : <span>Pick a date</span>}
+						className={cn(
+							'w-[300px] justify-start text-left font-normal',
+							!date && 'text-muted-foreground'
+						)}>
+						<CalendarIcon />
+						{date?.from ? (
+							date.to ? (
+								<>
+									{format(date.from, 'LLL dd, y')} -{' '}
+									{format(date.to, 'LLL dd, y')}
+								</>
+							) : (
+								format(date.from, 'LLL dd, y')
+							)
+						) : (
+							<span>Pick a date</span>
+						)}
 					</Button>
 				</PopoverTrigger>
 				<PopoverContent
-					className='w-auto p-0'
-					align='start'
-				>
+					className="w-auto p-0"
+					align="start">
 					<Calendar
-						mode='single'
-						selected={date ?? new Date()}
-						onSelect={(e) => (e ? setDate && setDate(e) : undefined)}
+						initialFocus
+						mode="range"
+						defaultMonth={date?.from}
+						selected={date}
+						onSelect={setDate}
+						numberOfMonths={2}
 					/>
 				</PopoverContent>
 			</Popover>
-			<input
-				type='hidden'
-				name={name}
-				value={date?.toISOString()}
-			/>
-		</>
+		</div>
 	);
-};
+}
 
-export default DatePicker;
+export function DatePickerWithPresets({
+	from,
+	to,
+}: {
+	from?: Date;
+	to?: Date;
+}) {
+	const today = React.useMemo(() => new Date(), []);
+	const [date, setDate] = React.useState<DateRange | undefined>({
+		from: from ?? subDays(today, 20),
+		to: to ?? today,
+	});
+
+	const pathname = usePathname();
+	const { push } = useRouter();
+
+	React.useEffect(() => {
+		if (!date?.from || !date?.to) return;
+		if (
+			date.from.getDate() === subDays(today, 20).getDate() &&
+			today.getDate() === date.to.getDate()
+		)
+			return;
+		const params = new URLSearchParams();
+		params.set('start_date', date.from.toISOString());
+		params.set('end_date', date.to.toISOString());
+
+		push(pathname + '?' + params.toString());
+	}, [date, pathname, push, today]);
+
+	return (
+		<Popover>
+			<PopoverTrigger asChild>
+				<Button
+					id="date"
+					variant={'outline'}
+					className={cn(
+						'w-[300px] justify-start text-left font-normal',
+						!date && 'text-muted-foreground'
+					)}>
+					<CalendarIcon className="mr-3" />
+					{date?.from ? (
+						date.to ? (
+							<>
+								{format(date.from, 'LLL dd, y')} -{' '}
+								{format(date.to, 'LLL dd, y')}
+							</>
+						) : (
+							format(date.from, 'LLL dd, y')
+						)
+					) : (
+						<span>Pick a date</span>
+					)}
+				</Button>
+			</PopoverTrigger>
+
+			<PopoverContent
+				className="flex w-auto flex-col space-y-1.5 p-1.5"
+				align="start">
+				<h3 className="tracking-tight font-medium text-sm text-muted-foreground">
+					Presets
+				</h3>
+
+				<div className="grid grid-cols-3 gap-3 px-1.5">
+					<Button
+						variant="outline"
+						onClick={() => {
+							setDate({
+								from: subWeeks(today, 1),
+								to: today,
+							});
+						}}>
+						This Week
+					</Button>
+
+					<Button
+						variant="outline"
+						onClick={() => {
+							setDate({
+								from: startOfMonth(today),
+								to: endOfMonth(today),
+							});
+						}}>
+						This Month
+					</Button>
+
+					<Button
+						variant="outline"
+						onClick={() => {
+							setDate({
+								from: startOfQuarter(today),
+								to: endOfQuarter(today),
+							});
+						}}>
+						This Quarter
+					</Button>
+
+					{/* <Button
+						variant="outline"
+						onClick={() => {
+							setDate({
+								from: subDays(todaysDate, 365),
+								to: todaysDate,
+							});
+						}}>
+						Past Year
+					</Button> */}
+				</div>
+
+				{/* <h3 className="tracking-tight font-medium text-sm text-muted-foreground">
+					Custom
+				</h3> */}
+
+				{/* <div className="rounded-md border">
+				</div> */}
+				<Calendar
+					initialFocus
+					mode="range"
+					defaultMonth={date?.from}
+					selected={date}
+					onSelect={setDate}
+					numberOfMonths={2}
+				/>
+			</PopoverContent>
+		</Popover>
+	);
+}
